0b05e206e86c3be80dea62c9125d14fb8467ee2c
PHEARUN PHAL <mrphearun.2024@gmail.com>
2024-12-14 03:06:20 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.getElementById('navToggle');
-const navLinks = document.querySelector('.nav-links');
-
-navToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
